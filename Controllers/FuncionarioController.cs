8d4fdc2a39a75760b418155e5ef74aeee18967e6
Luciene Oliveira <Luciene Oliveira@CASA2>
2024-06-22 16:38:49 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/FuncionarioController.cs
@@ -0,0 +1,87 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+
+namespace StefaniniPonto2.Controllers
+{
+    public class FuncionarioController : Controller
+    {
+        //
+        // GET: /Funcionario/
+        public ActionResult Index()
+        {
+            StefaniniProntoEntities entities = new StefaniniProntoEntities();
+
+            ViewBag.PontoList = entities.Funcionario.ToList();
+
+            return View();
+        }
+
+        
+    public ActionResult NovoFuncionario(Funcionario funcionario)
+        {
+        if (funcionario.Nome == null)
+            {
+                ViewBag.MensagemErro = "Preencha o Nome";
+            }
+            else if (funcionario.Salario == 0)
+            { 
+                ViewBag.MensagemErro = "Preencha o Email";
+            }
+
+        if (ViewBag.MensagemErro == null)
+        {
+            StefaniniProntoEntities entities = new StefaniniProntoEntities();
+
+            entities.Funcionario.Add(funcionario);
+            entities.SaveChanges();
+
+            ViewData.ModelState.Clear();
+
+            ViewBag.MensagemSucesso = "Funcionário cadastrado com sucesso!";
+        }
+
+        return View();
+         
+        }
+
+    public ActionResult Editar(int Id)
+    {
+        StefaniniProntoEntities entities = new StefaniniProntoEntities();
+
+        Funcionario funcionario = entities.Funcionario.Find(Id);
+
+        return View(funcionario);
+
+    }
+
+    public ActionResult Gravar(Funcionario funcionario)
+    {
+
+        if (funcionario.Nome == null)
+        {
+            ViewBag.MensagemErro = "Preencha o Nome";
+        }
+        else if (funcionario.Salario == 0)
+        {
+            ViewBag.MensagemErro = "Preencha o salario";
+        }
+        
+        if (ViewBag.MensagemErro == null)
+        {
+            StefaniniProntoEntities entities = new StefaniniProntoEntities();
+
+            entities.Entry(funcionario).State = EntityState.Modified;
+            entities.SaveChanges();
+            ViewBag.MensagemSucesso = "Funcionario alterado co sucesso!";
+        }
+
+
+        return View("Editar");
+    }
+	}
+}
---
+++ Controllers/PontoController.cs
@@ -0,0 +1,24 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+
+namespace StefaniniPonto2.Controllers
+{
+    public class PontoController : Controller
+    {
+        //
+        // GET: /Ponto/
+        public ActionResult Index()
+        {
+            StefaniniProntoEntities entities = new StefaniniProntoEntities();
+
+            ViewBag.PontoList = entities.Ponto.ToList();
+
+            return View();
+        }
+	}
+}
