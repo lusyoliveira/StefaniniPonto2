83965b58e90feb04e536bde33834ee57f0b5c928
Luciene Oliveira <Luciene Oliveira@CASA2>
2024-10-06 12:23:05 -0300

    Atualizando o projeto

---
+++ SQLQuery1.sql
@@ -0,0 +1,14 @@
+﻿CREATE DATABASE StefaniniPronto;
+
+CREATE TABLE Funcionario (Id INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
+             Nome NVARCHAR (250) NOT NULL,
+			 Salario DECIMAL(7,2) NOT NULL);
+
+CREATE TABLE Ponto (Id INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
+             IdFuncionario	INT NOT NULL,
+			 DataHora DATETIME NOT NULL,
+			 FOREIGN KEY (IdFuncionario) REFERENCES Funcionario(Id));
+
+INSERT INTO Funcionario (Nome, Salario) VALUES ('Luciene', 900);
+INSERT INTO Ponto (IdFuncionario, DataHora) VALUES (1, '19:07:16');
+
